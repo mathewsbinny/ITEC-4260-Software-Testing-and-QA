e6bc0787d8553a1bf1f1459223a3de666b2e59fb
mathewsbinny <mathewsb.00@gmail.com>
2024-09-19 13:26:49 -0400

    Rest of assignment + homework problems

--- tst/TuitionCalculatorTest.java
+++ tst/TuitionCalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -23,15 +24,39 @@ public class TuitionCalculatorTest {
     public void testInstateTuition() {
         //5,262 per year (two semesters) from US NEWS
         //take average of 12,13,14,15 hours
-        
+
         driver.get(TUITION_CALC_URL);
+        WebElement inStateRadio = driver.findElement(By.id("inorout1"));
+        inStateRadio.click();
+        int twelveHoursCost = getSemesterCostPerHour("12 hours");
+        int thirteenHoursCost = getSemesterCostPerHour("13 hours");
+        int fourteenHoursCost = getSemesterCostPerHour("14 hours");
+        int fifteenHoursCost = getSemesterCostPerHour("15 hours");
+        double average = (twelveHoursCost + thirteenHoursCost + fourteenHoursCost + fifteenHoursCost / 4);
+        System.out.println(average);
+        double expected = 5262.0;
+        Assert.assertEquals(expected, average*2, 350);
+    }
+
+    public static int getSemesterCostPerHour(String hours) {
         WebElement inStateRadio = driver.findElement(By.id("inorout1"));
         inStateRadio.click();
         WebElement creditHours = driver.findElement(By.id("creditHOURS"));
         Select creditHoursSelect = new Select(creditHours);
-        creditHoursSelect.selectByVisibleText("12 hours");
+        creditHoursSelect.selectByVisibleText(hours);
         WebElement semesterCost = driver.findElement(By.id("totalcost"));
-        System.out.println(semesterCost.getAttribute("value"));
+        String cost = semesterCost.getAttribute("value");
+        return Integer.parseInt(cost.substring(1, cost.length()-3));
+    }
+
+    //TODO:
+    public void testOutOfStateTuition() {
+
+    }
+
+    //TODO:
+    public void testFall2024TuitionBill() {
+
     }
 
     @AfterClass
