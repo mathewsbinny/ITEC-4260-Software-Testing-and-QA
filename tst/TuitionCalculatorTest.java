d1fbd99a87524568ad260268fbd511c493f29f54
mathewsbinny <mathewsb.00@gmail.com>
2024-10-02 22:32:21 -0400

    Added Assignment 4

--- tst/TuitionCalculatorTest.java
+++ tst/TuitionCalculatorTest.java
@@ -11,13 +11,22 @@ import org.openqa.selenium.support.ui.Select;
 
 public class TuitionCalculatorTest {
 
-    private static final String TUITION_CALC_URL = ("https://www.usnews.com/best-colleges/georgia-gwinnett-college-41429");
     private static WebDriver driver;
+    private final String TUITION_CALC_URL = "https://www.ggc.edu/admissions/tuition-and-financial-aid-calculators";
 
     @BeforeClass
-    public static void setUp(){
-        System.setProperty("webdriver.chrome.drive", "chromedriver.exe");
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
+    }
+
+    private static int getSemesterCostPerHour(String hours) {
+        WebElement creditHours = driver.findElement(By.id("creditHOURS"));
+        Select creditHoursSelect = new Select(creditHours);
+        creditHoursSelect.selectByVisibleText(hours);
+        WebElement semesterCost = driver.findElement(By.id("totalcost"));
+        String cost = semesterCost.getAttribute("value");
+        return Integer.parseInt(cost.substring(1,cost.length()-3));
     }
 
     @Test
@@ -36,31 +45,42 @@ public class TuitionCalculatorTest {
         System.out.println(average);
         double expected = 5262.0;
         Assert.assertEquals(expected, average*2, 350);
-    }
 
-    private static int getSemesterCostPerHour(String hours) {
-        WebElement creditHours = driver.findElement(By.id("creditHOURS"));
-        Select creditHoursSelect = new Select(creditHours);
-        creditHoursSelect.selectByVisibleText(hours);
-        WebElement semesterCost = driver.findElement(By.id("totalcost"));
-        String cost = semesterCost.getAttribute("value");
-        return Integer.parseInt(cost.substring(1,cost.length()-3));
     }
 
     @Test
     public void testOutOfStateTuition() {
-
+        driver.get(TUITION_CALC_URL);
+        WebElement outStateRadio = driver.findElement(By.id("inorout0"));
+        outStateRadio.click();
+        int twelveHoursCost = getSemesterCostPerHour("12 hours");
+        int thirteenHoursCost = getSemesterCostPerHour("13 hours");
+        int fourteenHoursCost = getSemesterCostPerHour("14 hours");
+        int fifteenHoursCost = getSemesterCostPerHour("15 hours");
+        double average = (twelveHoursCost + thirteenHoursCost + fourteenHoursCost + fifteenHoursCost)/4.0;
+        System.out.println(average);
+        double expected = 16348.0;
+        Assert.assertEquals(expected, average*2, 1000);
     }
 
     @Test
     public void testFall2024TuitionBill() {
         //$2000
+        driver.get(TUITION_CALC_URL);
+        WebElement inStateRadio = driver.findElement(By.id("inorout1"));
+        inStateRadio.click();
+        int twelveHoursCost = getSemesterCostPerHour("12 hours");
+        double estimatedCost = twelveHoursCost;
+        System.out.println(estimatedCost);
+        double expected = 2388.33;
+        Assert.assertEquals(expected, estimatedCost, 500);
     }
 
 
     @AfterClass
     public static void tearDown() throws InterruptedException {
-        // Thread.sleep(6000);
-        // driver.close();
+        Thread.sleep(6000);
+        driver.close();
     }
+
 }
