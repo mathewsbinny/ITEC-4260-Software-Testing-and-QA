636d49d8e48de61311e635f107d5e8121de1bc85
mathewsbinny <mathewsb.00@gmail.com>
2024-10-17 13:31:12 -0400

    Added Assignment 7 with Mockito

--- tst/InventoryMockTest.java
+++ tst/InventoryMockTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -6,7 +7,25 @@ public class InventoryMockTest {
     @Test
     public void testCheapestGame() {
         InventoryInterface inv = Mockito.mock(InventoryInterface.class);
-        
+        Game g = Mockito.mock(Game.class);
+        Mockito.when(g.getName()).thenReturn("Madden NFL");
+        Mockito.when(inv.findCheapestGame()).thenReturn(g);
+        Assert.assertEquals("Madden NFL", inv.findCheapestGame().getName());
     }
 
+    @Test
+    public void testMostExpensiveGame() {
+        InventoryInterface inv = Mockito.mock(InventoryInterface.class);
+        Game g = Mockito.mock(Game.class);
+        Mockito.when(g.getName()).thenReturn("Super Mario Bros");
+        Mockito.when(inv.findMostExpensiveGame()).thenReturn(g);
+        Assert.assertEquals("Super Mario Bros", inv.findMostExpensiveGame().getName());
+    }
+
+    @Test
+    public void testAveragePriceOfAllGames() {
+        InventoryInterface inv = Mockito.mock(InventoryInterface.class);
+        Mockito.when(inv.getAveragePriceOfAllGames()).thenReturn(60.0);
+        Assert.assertEquals(60.0, inv.getAveragePriceOfAllGames(), 0);
+    }
 }
